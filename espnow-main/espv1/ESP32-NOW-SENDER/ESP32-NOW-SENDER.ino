a2412f5526de87881501ad2e43554515c790a75d
Halit Buğra Yıldırım <halit8484@yandex.com>
2023-05-13 00:10:43 +0300

    comment

--- espnow-main/espv1/ESP32-NOW-SENDER/ESP32-NOW-SENDER.ino
+++ espnow-main/espv1/ESP32-NOW-SENDER/ESP32-NOW-SENDER.ino
@@ -1,30 +1,29 @@
 
-//update mac addresses
-// SENDER MAC ADDRESS -> // uint8_t broadcastAddress[] = {0x30, 0xC6, 0xF7, 0x03, 0xFC, 0x94};  // 0 has damage
-// RECEIVER MAC ADDRESS -> // uint8_t broadcastAddress[] = {0x30, 0xC6, 0xF7, 0x04, 0x25, 0x10};  // 4 has damage
 /*
 
 */
 
-#include <esp_now.h>
+#include <esp_now.h>     //haberleşme için gerekli kütüphaneler
 #include <WiFi.h>
-#include <SPI.h>
+#include <SPI.h>       // spi haberleşme kütüphanesi
 #include <MFRC522.h>
-#include<Preferences.h>
+#include<Preferences.h>    // flash memory'ye kayıt fonk için kütüphane
 
-#define led_halit 4
+#define led_halit 4  // kişileri gösteren led tanımları
 #define led_emirhan 16
 #define led_buse 17
 
-#define SS_PIN  5  // ESP32 pin GIOP5 
+#define SS_PIN  5  // ESP32 pin GIOP5           //rfid spi habeleşme pinleri
 #define RST_PIN 27 // ESP32 pin GIOP27 
 MFRC522 rfid(SS_PIN, RST_PIN);
 
-/* RECEIVER MAC Address */
-uint8_t broadcastAddress[] = {0xC8, 0xF0, 0x9E, 0x9E, 0x1A, 0x6C};
 
-/* Structure example to send data. */
-/* Must match the receiver structure. */
+
+
+/* RECEIVER MAC Address */
+uint8_t broadcastAddress[] = {0xC8, 0xF0, 0x9E, 0x9E, 0x1A, 0x6C};   // gönderimi yaptığımız cihazın mac adresi
+
+// göndereceğimiz verinin yapısı
 typedef struct struct_message {
     bool halit;
     bool emirhan;
@@ -35,48 +34,53 @@ Preferences preferences_nesnesi1;  //flash memory'ye yazilacak kod icin degisken
 Preferences preferences_nesnesi2;
 Preferences preferences_nesnesi3;
 
-/* Create a struct_message called myData */
+
+
+// yeni tanımladığımız veri tipinde bir değişken tanımlıyoruz
 struct_message myData;
 
 esp_now_peer_info_t peerInfo;
 
+
+
+
 /* Callback when data is sent. */
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   Serial.print("\r\nLast Packet Send Status:\t");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");  // veri gönderilemediğinde yazdırılacak kod
 }
  
 void setup() {  
   /* Init Serial Monitor. */
   Serial.begin(9600); // Serial.begin(115200);
   
-  SPI.begin(); // init SPI bus
+  SPI.begin();             //rfid haberleşme başlangıcı için gerekli kodlar
   rfid.PCD_Init(); // init MFRC522
   Serial.println("Tap an RFID/NFC tag on the RFID-RC522 reader");
   
-  pinMode(led_halit, OUTPUT);
+  pinMode(led_halit, OUTPUT);               // led için ayarlanan pinler çıkış olarak ayarlanıyor
   pinMode(led_emirhan, OUTPUT);
   pinMode(led_buse, OUTPUT);
-    
-  /* Set device as a Wi-Fi Station. */
+
+
+
+
+// wifi mode ayarı yapılıyor  esp_now kütüphanesinden
   WiFi.mode(WIFI_STA);
 
-  /* Init ESP-NOW. */
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
 
-  /* Once ESPNow is successfully Init, we will register for Send CB to */
-  /* get the status of Trasnmitted packet. */
+// veri gönderildiğinde çalıştırılacak kod için esp_now kütüphanesinden bir fonk ile register tanımı yapılıyor
   esp_now_register_send_cb(OnDataSent);
   
-  /* Register peer. */
+
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
   peerInfo.channel = 0;  
   peerInfo.encrypt = false;
-  
-  /* Add peer. */
+ 
   if (esp_now_add_peer(&peerInfo) != ESP_OK){
     Serial.println("Failed to add peer");
     return;
@@ -86,6 +90,8 @@ void setup() {
   preferences_nesnesi1.begin("halit_flash", false);
   preferences_nesnesi2.begin("emirhan_flash", false);
   preferences_nesnesi3.begin("buse_flash", false);
+
+  
   //evde olup olmama durumu flash memoriden cekiliyor, baglanti olmadiginda calisicak
   myData.halit = preferences_nesnesi1.getBool("durum", false);
   myData.emirhan = preferences_nesnesi2.getBool("durum", false);
@@ -93,6 +99,7 @@ void setup() {
   bool halit = myData.halit;
   bool emirhan = myData.emirhan;
   bool buse = myData.buse;
+  
   digitalWrite(led_halit, halit);
   digitalWrite(led_emirhan, emirhan);
   digitalWrite(led_buse, buse);
@@ -101,7 +108,7 @@ void setup() {
  
 void loop() {
   /* Send message via ESP-NOW. */
-  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));    // saniyede 1 defa veri gönderimi yapılıyor
   delay(1000);
 
       //gönderme durumunu belirten kod bloğu, seri monitör kalabalığından devre dışı bırakıldı
@@ -112,7 +119,11 @@ void loop() {
   //   Serial.println("Error sending the data");
   // }
 
-  //halit id 
+
+
+
+                                  
+  //halit id                 tanımlanan rfıd çiplerin id leri tanımlanıyor
   byte ID1[4] = {
   243,
   214,
@@ -134,7 +145,7 @@ void loop() {
   212
   };
 
-  //rfid hex dönüştürücü
+  //rfid hex dönüştürücü                   hex olarak görüntüleme için eklenmişti devre dışı bırakıldı
   // if (rfid.PICC_IsNewCardPresent()) { // new tag is available 
   //   if (rfid.PICC_ReadCardSerial()) { // NUID has been readed
   //     MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
@@ -170,12 +181,12 @@ void loop() {
     ekranaYazdir();
     if(myData.halit == 0){
       myData.halit = 1;
-      Serial.println("Hoşgeldin Hanlar Hanı Cihan Padişahı Halit Han Hazretleri");           
+      Serial.println("Halit Buğra");           
     }
     else if(myData.halit == 1)
     {
       myData.halit = 0;
-      Serial.println("Hoşçakal Hanlar Hanı Cihan Padişahı Halit Han Hazretleri");
+      Serial.println("Hoşçakal Halit Buğra");
     }  
   }        
   else if(rfid.uid.uidByte[0] == ID2[0] && //Okunan kart ID'si ile ID değişkenini karşılaştırıyoruz.
@@ -211,7 +222,7 @@ void loop() {
     }
   }
   
-  else { //Yetkisiz girişte içerideki komutlar çalıştırılır.
+  else { //bilinmeyen kart durumu
     Serial.println("Bilinmeyen Kart");
     ekranaYazdir();
   }
@@ -224,7 +235,7 @@ void loop() {
   digitalWrite(led_buse, myData.buse); 
   delay(1); 
 
-  // //durum kodları
+  // //durum kodları      kalabalığı engellemek için devre dışı bırakıldı                          
   // Serial.print("Bytes received: ");
   // Serial.println(len);
   // Serial.print("Halit evde mi:" );
@@ -235,7 +246,7 @@ void loop() {
   // Serial.println(myData.buse); 
 }
 
-// id seri monitöre yazdırma kodu
+// id seri monitöre yazdırma kodu   tanımlama sırasında id görüntüleme için kullanılan kod
 void ekranaYazdir() {
   Serial.print("ID Numarasi: ");
   for (int sayac = 0; sayac < 4; sayac++) {
